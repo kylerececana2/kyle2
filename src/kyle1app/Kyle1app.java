ef56cfcee66af335229766aeb8bdf7c51ada32c7
kylerececana2 <kylerecana2@gmail.com>
2025-08-01 14:24:05 +0800

    pardy

---
+++ src/kyle1app/Kyle1app.java
@@ -0,0 +1,27 @@
+
+package kyle1app;
+
+import java.util.Scanner;
+
+
+public class Kyle1app {
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+       String name;
+       int byear;
+       
+       System.out.println("Enter your name: ");
+       name = sc.nextLine();
+       System.out.println("Enter your birth year: ");
+       byear = sc.nextInt();
+       
+       System.out.println("Hello "+name+ " your age is "+(2025-byear));
+               
+    }
+    
+}
+    
+    
+
---
+++ src/kyle1app/activity2.java
@@ -0,0 +1,9 @@
+package kyle1app;
+
+
+public class activity2 {
+    
+    public static void main (String[] args){
+       System.out.println("Enter Hello Activiry 2: ");
+    }
+}
